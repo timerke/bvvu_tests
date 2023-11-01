e7dd8be908e3d1a07660c65e28382631def70137
timerke <timerke@mail.ru>
2023-11-01 13:31:36 +0300

    Fixed analyzer

--- usb_slot_test/analyzer/analyzer.py
+++ usb_slot_test/analyzer/analyzer.py
@@ -10,12 +10,15 @@ import matplotlib.pyplot as plt
 
 class Analyzer:
 
-    PATTERN = re.compile(r"^\[(.*) INFO\] \[(UIOB|SSH)\] Number of missing modules: (\d+), missing modules: \[(.*)\]$")
+    PATTERN = re.compile(r"^\[(.*) INFO\] \[(UIOB|SSH_DEV|SSH_DEV_XIMC)\] Number of missing modules: (\d+), "
+                         r"missing modules: \[(.*)\]$")
     SLOT_NUMBER: int = 16
 
     def __init__(self) -> None:
-        self._ssh_missing_slot_history: List[List[datetime]] = [[] for _ in range(Analyzer.SLOT_NUMBER)]
-        self._ssh_slot_history: List[List[datetime]] = [[] for _ in range(Analyzer.SLOT_NUMBER)]
+        self._ssh_dev_missing_slot_history: List[List[datetime]] = [[] for _ in range(Analyzer.SLOT_NUMBER)]
+        self._ssh_dev_slot_history: List[List[datetime]] = [[] for _ in range(Analyzer.SLOT_NUMBER)]
+        self._ssh_dev_ximc_missing_slot_history: List[List[datetime]] = [[] for _ in range(Analyzer.SLOT_NUMBER)]
+        self._ssh_dev_ximc_slot_history: List[List[datetime]] = [[] for _ in range(Analyzer.SLOT_NUMBER)]
         self._uiob_missing_module_history: List[List[datetime]] = [[] for _ in range(Analyzer.SLOT_NUMBER)]
         self._uiob_module_history: List[List[datetime]] = [[] for _ in range(Analyzer.SLOT_NUMBER)]
 
@@ -35,13 +38,18 @@ class Analyzer:
             result = Analyzer.PATTERN.match(record)
             if result:
                 log_time = datetime.strptime(result.group(1), "%Y-%m-%d %H:%M:%S")
-                if "UIOB" in record:
-                    self._get_modules_from_uiob_record(result, log_time)
-                elif "SSH" in record:
+                if "[UIOB]" in record:
+                    self._get_modules_from_record(result, log_time, self._uiob_module_history,
+                                                  self._uiob_missing_module_history)
+                elif "[SSH_DEV]" in record:
                     self._get_slots_from_ssh_record(result, log_time)
+                elif "[SSH_DEV_XIMC]" in record:
+                    self._get_modules_from_record(result, log_time, self._ssh_dev_ximc_slot_history,
+                                                  self._ssh_dev_ximc_missing_slot_history)
 
     @staticmethod
-    def _draw_data(enabled: List[List[datetime]], disabled: List[List[datetime]], slot: bool) -> None:
+    def _draw_data(enabled: List[List[datetime]], disabled: List[List[datetime]], legend_format: str,
+                   y_labels: List[str]) -> None:
         """
         :param enabled: history of modules in time when they were in working condition;
         :param disabled: history of modules in time when they were inactive;
@@ -59,11 +67,8 @@ class Analyzer:
 
             dump_percentage = round(100 * len(d_times) / (len(e_times) + len(d_times)), 2)
             index += 1
-            if slot:
-                label = f"Слот #{index} ({dump_percentage}% отвалов)"
-            else:
-                label = f"Модуль #{index} ({dump_percentage}% отвалов)"
-            axs[0].scatter(e_times, len(e_times) * [index], label=label)
+            axs[0].scatter(e_times, len(e_times) * [index], label=legend_format.format(index=index,
+                                                                                       dump_percentage=dump_percentage))
             axs[1].scatter(d_times, len(d_times) * [index])
 
             times = [*e_times, *d_times]
@@ -74,14 +79,8 @@ class Analyzer:
             if total_max_x is None or total_max_x < local_max_x:
                 total_max_x = local_max_x
 
-        if slot:
-            y_label_1 = "Слоты по ssh"
-            y_label_2 = "Отвалившиеся слоты"
-        else:
-            y_label_1 = "Модули в админке"
-            y_label_2 = "Отвалившиеся модули"
-        axs[0].set_ylabel(y_label_1)
-        axs[1].set_ylabel(y_label_2)
+        for i, y_label in enumerate(y_labels):
+            axs[i].set_ylabel(y_label)
 
         start_date = get_start_date(*enabled, *disabled)
         for ax in axs:
@@ -93,30 +92,34 @@ class Analyzer:
         axs[0].legend(bbox_to_anchor=(0.1, 1.3), loc="upper left", ncol=4)
         plt.show()
 
-    def _get_modules_from_uiob_record(self, result, log_time: datetime) -> None:
+    @staticmethod
+    def _get_modules_from_record(result, log_time: datetime, history: List[List[datetime]],
+                                 missing_history: List[List[datetime]]) -> None:
         missing_modules = []
         for module in result.group(4).split(", "):
             if module:
+                module = module.strip("'")
                 missing_modules.append(int(module))
 
         for module_index in range(1, Analyzer.SLOT_NUMBER + 1):
             if module_index in missing_modules:
-                self._uiob_missing_module_history[module_index - 1].append(log_time)
+                missing_history[module_index - 1].append(log_time)
             else:
-                self._uiob_module_history[module_index - 1].append(log_time)
+                history[module_index - 1].append(log_time)
 
     def _get_slots_from_ssh_record(self, result, log_time: datetime) -> None:
         missing_slots = []
+        pattern = re.compile(r"^'ttyACM(?P<index>\d+)'$")
         for slot in result.group(4).split(", "):
-            if slot:
-                slot = slot.strip("'")
-                missing_slots.append(int(slot))
+            new_result = pattern.match(slot)
+            if new_result:
+                missing_slots.append(int(new_result["index"]))
 
-        for slot_index in range(1, Analyzer.SLOT_NUMBER + 1):
+        for slot_index in range(Analyzer.SLOT_NUMBER):
             if slot_index in missing_slots:
-                self._ssh_missing_slot_history[slot_index - 1].append(log_time)
+                self._ssh_dev_missing_slot_history[slot_index].append(log_time)
             else:
-                self._ssh_slot_history[slot_index - 1].append(log_time)
+                self._ssh_dev_slot_history[slot_index].append(log_time)
 
     def run(self, log_file: str) -> None:
         """
@@ -124,8 +127,15 @@ class Analyzer:
         """
 
         self._analyze_log(log_file)
-        self._draw_data(self._uiob_module_history, self._uiob_missing_module_history, slot=False)
-        self._draw_data(self._ssh_slot_history, self._ssh_missing_slot_history, slot=True)
+        self._draw_data(self._uiob_module_history, self._uiob_missing_module_history,
+                        legend_format="Модуль #{index} ({dump_percentage}% отвалов)",
+                        y_labels=["Модули в админке", "Отвалившиеся модули"])
+        self._draw_data(self._ssh_dev_ximc_slot_history, self._ssh_dev_ximc_missing_slot_history,
+                        legend_format="Слот #{index} ({dump_percentage}% отвалов)",
+                        y_labels=["Слоты по ssh (/dev/ximc)", "Отвалившиеся слоты"])
+        self._draw_data(self._ssh_dev_ximc_slot_history, self._ssh_dev_ximc_missing_slot_history,
+                        legend_format="Слот ttyACM{index} ({dump_percentage}% отвалов)",
+                        y_labels=["Слоты по ssh (/dev)", "Отвалившиеся слоты"])
 
 
 def get_start_date(*args) -> str:
